290a8dc98397b5f71413943f3a4bec34648cb9fc
Mulanga-hub <ST10522232@rcconnect.edu.za>
2026-04-11 17:23:20 +0000

    3:Completed registration and login glow

--- Main.java
+++ Main.java
@@ -9,28 +9,39 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("=== USER REGISTRATION ===");
 
         System.out.print("Enter username: ");
-        String username = input.nextLine();
+        String username = scanner.nextLine();
 
         System.out.print("Enter password: ");
-        String password = input.nextLine();
+        String password = scanner.nextLine();
 
         System.out.print("Enter cell number (+27...): ");
-        String cell = input.nextLine();
+        String cell = scanner.nextLine();
 
-        // register user and show result
         System.out.println(registerUser(username, password, cell));
+
+        //LOGIN SECTION
+        System.out.println("\n=== USER LOGIN ===");
+
+        System.out.println("Enter username: ");
+        String loginUsername = scanner.nextLine();
+
+        System.out.println("Enter password: ");
+        String loginPassword = scanner.nextLine();
+
+        boolean loginSuccess = loginUser(loginUsername, loginPassword);
+        System.out.println(returnLoginStatus(loginSuccess));
 
         System.out.println("User program finished");
 
-        input.close();
+        scanner.close();
     }
 
-    public static String registerUser(String username, String password, String cell) {
+    public static String registerUser(String username, String password, String cell){
 
         if (!Validation.checkUsername(username)) {
             return "Username is not correctly formatted";
@@ -44,11 +55,22 @@ public class Main {
             return "Cell phone number incorrectly formatted";
         }
 
-        // store data
         registeredUsername = username;
         registeredPassword = password;
         registeredCell = cell;
 
         return "User successfully registered";
+    }
+
+    public static boolean loginUser(String username, String password) {
+        return username.equals(registeredUsername) && password.equals(registeredPassword);
+    }
+
+    public static String returnLoginStatus(boolean status) {
+        if (status) {
+            return "Welcome, it is great to see you again.";
+        } else {
+            return "Username or password incorrect, please try again.";
+        }
     }
 }
