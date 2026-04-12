1bb5cf33a684c3e3837712db09ad38f5221da416
Mulanga-hub <ST10522232@rcconnect.edu.za>
2026-04-12 16:15:30 +0000

    2:Register feature completed

--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Main {
 
-    // store registered user details
     static String registeredUsername;
     static String registeredPassword;
     static String registeredCell;
@@ -24,24 +23,12 @@ public class Main {
 
         System.out.println(registerUser(username, password, cell));
 
-        //LOGIN SECTION
-        System.out.println("\n=== USER LOGIN ===");
-
-        System.out.println("Enter username: ");
-        String loginUsername = scanner.nextLine();
-
-        System.out.println("Enter password: ");
-        String loginPassword = scanner.nextLine();
-
-        boolean loginSuccess = loginUser(loginUsername, loginPassword);
-        System.out.println(returnLoginStatus(loginSuccess));
-
         System.out.println("User program finished");
 
         scanner.close();
     }
 
-    public static String registerUser(String username, String password, String cell){
+    public static String registerUser(String username, String password, String cell) {
 
         if (!Validation.checkUsername(username)) {
             return "Username is not correctly formatted";
@@ -60,17 +47,5 @@ public class Main {
         registeredCell = cell;
 
         return "User successfully registered";
-    }
-
-    public static boolean loginUser(String username, String password) {
-        return username.equals(registeredUsername) && password.equals(registeredPassword);
-    }
-
-    public static String returnLoginStatus(boolean status) {
-        if (status) {
-            return "Welcome, it is great to see you again.";
-        } else {
-            return "Username or password incorrect, please try again.";
-        }
     }
 }
